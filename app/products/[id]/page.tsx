aa7abccb2719e55b453df39203d3cbb6a4521e16
Ewerton Oliveira <ewertonoliveira946@gmail.com>
2024-05-03 15:54:17 -0300

    feat: add product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,48 @@
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+import ProductImage from "./_components/product-image";
+import ProducDetails from "./_components/product-datails";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      {/*IMAGEM*/}
+      <ProductImage product={product} />
+
+      {/*titulo e preço*/}
+      <ProducDetails product={product} complementaryProducts={juices} />
+    </div>
+  );
+};
+
+export default ProductPage;
